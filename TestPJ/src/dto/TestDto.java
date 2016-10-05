ef48273c5d41ea906ff8e4fa9d0039ee78ddab57
han2nite@gmail.com <MARUKO@MARUKOandREON>
2016-10-05 23:49:19 +0900

    はじめてのコミット

---
+++ TestPJ/src/dto/TestDto.java
@@ -0,0 +1,24 @@
+package dto;
+
+public class TestDto {
+
+	String testName;
+
+	/**
+	 * testNameを取得します。
+	 * @return testName
+	 */
+	public String getTestName() {
+	    return testName;
+	}
+
+	/**
+	 * testNameを設定します。
+	 * @param testName testName
+	 */
+	public void setTestName(String testName) {
+	    this.testName = testName;
+	}
+
+
+}
